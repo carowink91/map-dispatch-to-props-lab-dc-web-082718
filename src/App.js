ccc3f3f001ea94ae48a923bbb0772239cefd6705
Angelica Winkler <carowink3@gmail.com>
2018-11-15 10:08:22 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+import { addRestaurant} from './actions/restaurants'
 
 export class App extends Component {
   render() {
@@ -12,5 +13,12 @@ export class App extends Component {
     );
   }
 };
+
+
+// const mapStateToProps = state => {
+//   return{
+//     restaurants: state.restaurants
+//   }
+// }
 
 export default App;
--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -17,13 +17,14 @@ export class RestaurantInput extends Component {
 
   handleOnLocationChange = event => {
     this.setState({
-      [event.target.id]: event.target.value
+      location: event.target.value
     });
   }
 
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -34,6 +35,7 @@ export class RestaurantInput extends Component {
             type="text"
             onChange={(event) => this.handleOnNameChange(event)}
             id="name"
+            value={this.state.name}
             placeholder="restaurant name" />
         </p>
         <p>
@@ -41,6 +43,7 @@ export class RestaurantInput extends Component {
             type="text"
             onChange={(event) => this.handleOnLocationChange(event)}
             id="location"
+            value={this.state.location}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -49,6 +52,19 @@ export class RestaurantInput extends Component {
   }
 };
 
+// const mapDispatchToProps = dispatch => {
+//   return{
+//     addRestaurant: () => {
+//       dispatch(addRestaurant())
+//     }
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     restaurants: state.restaurants;
+//   }
+// }
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, {addRestaurant})(RestaurantInput)
